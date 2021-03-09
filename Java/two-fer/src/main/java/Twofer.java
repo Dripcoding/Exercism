e7c9665f080713b073fbe72f089dd08b43d9bbc1
Dripcoding <dripcoding@gmail.com>
2021-03-08 22:13:19 -0800

    add solution for java twofer exercise

--- Java/two-fer/src/main/java/Twofer.java
+++ Java/two-fer/src/main/java/Twofer.java
@@ -1,5 +1,17 @@
 public class Twofer {
+
+    static final String NO_NAME_GIVEN_PHRASE = "One for you, one for me.";
+    static final String EMPTY_NAME_GIVEN_PHRASE = "One for , one for me.";
+
     public String twofer(String name) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (name == null) {
+            return NO_NAME_GIVEN_PHRASE;
+        }
+
+        if (name == "") {
+            return EMPTY_NAME_GIVEN_PHRASE;
+        }
+
+        return String.format("One for %s, one for me.", name);
     }
 }
