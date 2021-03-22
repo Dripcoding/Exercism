fba331b39a3ac91a897306dcdc734b3b4e32ac7b
Dripcoding <dripcoding@gmail.com>
2021-03-21 21:13:40 -0700

    refactor java Grains solution

--- Java/grains/src/main/java/Grains.java
+++ Java/grains/src/main/java/Grains.java
@@ -2,17 +2,19 @@ import java.math.BigInteger;
 
 class Grains {
 
+    static final BigInteger BIG_INTEGER_2 = BigInteger.valueOf(2);
+
     public BigInteger grainsOnSquare(final int square) {
         if (square < 1 || square > 64) {
             throw new IllegalArgumentException("square must be between 1 and 64");
         }
-        return BigInteger.valueOf(2).pow(square - 1);
+        return BIG_INTEGER_2.pow(square - 1);
     }
 
     public BigInteger grainsOnBoard() {
         BigInteger total = BigInteger.valueOf(0);
         for (int i = 0; i < 64; i++) {
-            total = total.add(BigInteger.valueOf(2).pow(i));
+            total = total.add(BIG_INTEGER_2.pow(i));
         }
         return total;
     }
