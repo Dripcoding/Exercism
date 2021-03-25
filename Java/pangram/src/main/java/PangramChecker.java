015b6f92a622f7bb5b524d7097911d5a785b2e19
Dripcoding <dripcoding@gmail.com>
2021-03-24 20:23:07 -0700

    add solution using stream for java pangram checker

--- Java/pangram/src/main/java/PangramChecker.java
+++ Java/pangram/src/main/java/PangramChecker.java
@@ -1,7 +1,10 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class PangramChecker {
+
+    public static final int LETTERS_IN_ALPHABET = 26;
 
     public boolean isPangram(String input) {
         if (input.length() == 0) {
@@ -16,4 +19,11 @@ public class PangramChecker {
         return true;
     }
 
+    public boolean isPangramStreams(String input) {
+        if (input.isEmpty()) {
+            return false;
+        }
+        long letterCount = input.toLowerCase().chars().distinct().filter(Character::isAlphabetic).count();
+        return letterCount == LETTERS_IN_ALPHABET;
+    }
 }
