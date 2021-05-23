710045bd26d6ed62a6a40f7cb64cfb119b15b3a7
Dripcoding <dripcoding@gmail.com>
2021-05-23 12:57:09 -0700

    add solution using loop for java protein-translation exercise

--- Java/protein-translation/src/main/java/ProteinTranslator.java
+++ Java/protein-translation/src/main/java/ProteinTranslator.java
@@ -1,8 +1,54 @@
+import java.util.ArrayList;
 import java.util.List;
 
 class ProteinTranslator {
 
+    public static int CODON_LENGTH = 3;
+
+    public enum Codons {
+        AUG("Methionine"),
+        UUU("Phenylalanine"),
+        UUC("Phenylalanine"),
+        UUA("Leucine"),
+        UUG("Leucine"),
+        UCU("Serine"),
+        UCC("Serine"),
+        UCA("Serine"),
+        UCG("Serine"),
+        UAU("Tyrosine"),
+        UAC("Tyrosine"),
+        UGU("Cysteine"),
+        UGC("Cysteine"),
+        UGG("Tryptophan"),
+        UAA("STOP"),
+        UAG("STOP"),
+        UGA("STOP");
+
+        private String value;
+
+        Codons(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+
+
     List<String> translate(String rnaSequence) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < rnaSequence.length(); i += CODON_LENGTH) {
+            String rnaSubstr = rnaSequence.substring(i, i + CODON_LENGTH);
+            Codons currentCodon = Codons.valueOf(rnaSubstr);
+
+            if (currentCodon.getValue().equals("STOP")) {
+                break;
+            }
+            result.add(currentCodon.getValue());
+        }
+
+        return result;
     }
 }
