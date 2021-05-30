596900aa95fa327af0c8cf161423ba86df07c0aa
Dripcoding <dripcoding@gmail.com>
2021-05-30 13:48:22 -0700

    add solution that reduces code duplication and conditionals for java beer-song

--- Java/beer-song/src/main/java/BeerSong.java
+++ Java/beer-song/src/main/java/BeerSong.java
@@ -4,8 +4,43 @@ public class BeerSong {
     public BeerSong() {
     }
 
+    // solution that reduces code duplication and conditionals
+    public String sing(int startVerse, int stopVerse) {
+        StringBuilder verseBuilder = new StringBuilder();
+
+        for (int i = startVerse; i > (startVerse - stopVerse); i--) {
+            verseBuilder.append(sing(i));
+        }
+
+        return verseBuilder.toString();
+    }
+
+    public String sing(int verse) {
+        StringBuilder verseBuilder = new StringBuilder();
+
+        if (verse == 0) {
+            verseBuilder.append("No more bottles of beer on the wall, no more bottles of beer.\n");
+            verseBuilder.append("Go to the store and buy some more, 99 bottles of beer on the wall.\n\n");
+        } else if (verse == 1) {
+            verseBuilder.append(String.format("%s bottle of beer on the wall, %s bottle of beer.\n", verse, verse));
+            verseBuilder.append("Take it down and pass it around, no more bottles of beer on the wall.\n\n");
+        } else if (verse == 2) {
+            verseBuilder.append("2 bottles of beer on the wall, 2 bottles of beer.\n");
+            verseBuilder.append("Take one down and pass it around, 1 bottle of beer on the wall.\n\n");
+        } else {
+            verseBuilder.append(String.format("%s bottles of beer on the wall, %s bottles of beer.\n", verse, verse));
+            verseBuilder.append(String.format("Take one down and pass it around, %s bottles of beer on the wall.\n\n", verse - 1));
+        }
+
+        return verseBuilder.toString();
+    }
+
+    public String singSong() {
+        return sing(99, 100);
+    }
+
     // naive solution
-    public String sing(int startVerse, int count) {
+    public String naiveSing(int startVerse, int count) {
         StringBuilder verseBuilder = new StringBuilder();
         int current = startVerse;
         for (int i = 0; i < count; i++) {
@@ -43,7 +78,7 @@ public class BeerSong {
         return verseBuilder.toString();
     }
 
-    public String singSong() {
-        return sing(99, 100);
+    public String singSongUsingNaiveSing() {
+        return naiveSing(99, 100);
     }
 }
