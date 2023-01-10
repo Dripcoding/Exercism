77f88134765edf68939f6b64c0a7b9ca4a359cc0
Dripcoding <dripcoding@gmail.com>
2023-01-10 00:14:59 -0800

    submit lasagna.py solution

--- Python/guidos-gorgeous-lasagna/lasagna.py
+++ Python/guidos-gorgeous-lasagna/lasagna.py
@@ -4,12 +4,14 @@ Learn about Guido, the creator of the Python language: https://en.wikipedia.org/
 """
 
 # TODO: define the 'EXPECTED_BAKE_TIME' constant
+EXPECTED_BAKE_TIME = 40
+
 # TODO: consider defining the 'PREPARATION_TIME' constant
 #       equal to the time it takes to prepare a single layer
-
+PREPARATION_TIME = 2
 
 # TODO: define the 'bake_time_remaining()' function
-def bake_time_remaining():
+def bake_time_remaining(elapsed_bake_time):
     """Calculate the bake time remaining.
 
     :param elapsed_bake_time: int - baking time already elapsed.
@@ -20,11 +22,31 @@ def bake_time_remaining():
     based on the `EXPECTED_BAKE_TIME`.
     """
 
-    pass
+    return EXPECTED_BAKE_TIME - elapsed_bake_time
 
 
 # TODO: define the 'preparation_time_in_minutes()' function
 #       and consider using 'PREPARATION_TIME' here
 
+def preparation_time_in_minutes(layers):
+    """Calculate the time it takes to prepare n layers.
+    
+    :param layers: int - number of layers in lasagna.
+    :return: int - total minutes of preparation derived from 'PREPARATION_TIME'
+
+    Function that takes the number of layers the lasagna and returns the time it takes to
+    prepare all layers.    
+    """
+    return layers * PREPARATION_TIME
+
 
 # TODO: define the 'elapsed_time_in_minutes()' function
+def elapsed_time_in_minutes(prep_time, bake_time):
+    """Calculates the total cooking time for the lasagna.
+    
+    :param prep_time: int - total time to prepare lasagna
+    :param bake_time: int - total baking time
+    
+    Function that sums the preparation and the baking time.
+    """
+    return prep_time * 2 + bake_time
