e8b874006723c7076a9107629e40acadd657e62e
Dripcoding <dripcoding@gmail.com>
2023-01-10 20:30:56 -0800

    fix linter warnings

--- Python/guidos-gorgeous-lasagna/lasagna.py
+++ Python/guidos-gorgeous-lasagna/lasagna.py
@@ -3,20 +3,14 @@
 Learn about Guido, the creator of the Python language: https://en.wikipedia.org/wiki/Guido_van_Rossum
 """
 
-# TODO: define the 'EXPECTED_BAKE_TIME' constant
 EXPECTED_BAKE_TIME = 40
 
-# TODO: consider defining the 'PREPARATION_TIME' constant
-#       equal to the time it takes to prepare a single layer
 PREPARATION_TIME = 2
 
-# TODO: define the 'bake_time_remaining()' function
 def bake_time_remaining(elapsed_bake_time):
     """Calculate the bake time remaining.
-
     :param elapsed_bake_time: int - baking time already elapsed.
     :return: int - remaining bake time (in minutes) derived from 'EXPECTED_BAKE_TIME'.
-
     Function that takes the actual minutes the lasagna has been in the oven as
     an argument and returns how many minutes the lasagna still needs to bake
     based on the `EXPECTED_BAKE_TIME`.
@@ -24,16 +18,10 @@ def bake_time_remaining(elapsed_bake_time):
 
     return EXPECTED_BAKE_TIME - elapsed_bake_time
 
-
-# TODO: define the 'preparation_time_in_minutes()' function
-#       and consider using 'PREPARATION_TIME' here
-
 def preparation_time_in_minutes(layers):
     """Calculate the time it takes to prepare n layers.
-    
     :param layers: int - number of layers in lasagna.
     :return: int - total minutes of preparation derived from 'PREPARATION_TIME'
-
     Function that takes the number of layers the lasagna and returns the time it takes to
     prepare all layers.    
     """
@@ -43,10 +31,8 @@ def preparation_time_in_minutes(layers):
 # TODO: define the 'elapsed_time_in_minutes()' function
 def elapsed_time_in_minutes(prep_time, bake_time):
     """Calculates the total cooking time for the lasagna.
-    
     :param prep_time: int - total time to prepare lasagna
     :param bake_time: int - total baking time
-    
     Function that sums the preparation and the baking time.
     """
     return prep_time * 2 + bake_time
