dd24a12aded913ce75ce66ed1183fb81d36248ac
Dripcoding <dripcoding@gmail.com>
2023-02-18 15:54:26 -0800

    submit isogram.py

--- Python/isogram/isogram.py
+++ Python/isogram/isogram.py
@@ -1,2 +1,12 @@
+from collections import Counter
+
 def is_isogram(string):
-    pass
+    if len(string) == 0: 
+        return True
+    
+    counter = Counter(string.lower().replace('-', '').replace(' ', ''))
+    
+    return all(val == 1 for val in counter.values())
+
+    
+        
