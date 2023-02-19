8629a3f423ff78569d70c1d2a6f76bd4a2a35402
Dripcoding <dripcoding@gmail.com>
2023-02-18 17:16:53 -0800

    isbn_verifier.py - add solution with regex

--- Python/isbn-verifier/isbn_verifier.py
+++ Python/isbn-verifier/isbn_verifier.py
@@ -1,4 +1,5 @@
 from string import ascii_uppercase
+from re import match
 
 def is_valid(isbn):
     isbn = isbn.replace('-', '')
@@ -21,6 +22,16 @@ def is_valid(isbn):
         
     return isbn_sum % 11 == 0
         
+
+def is_valid_with_regex(isbn):
+    isbn = (isbn or '').replace('-', '')
+    regex = r'^\d{9}[\dX]$'
+    if not match(regex, isbn):
+        return False
+    
+    digits = [(d == 'X' and 10) or int(d) for d in isbn]
+    return sum(d * (10 - i) for i, d in enumerate(digits)) % 11 == 0
+
     
 def validate_isbn(isbn):
     length = len(isbn)
